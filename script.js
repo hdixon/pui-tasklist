b0f4cdc70b8f653b99a985435f4929855cb41a6c
Henry Dixon <hdixon@cmu.edu>
2016-10-22 18:20:13 -0400

    clean up adding and removing, make everything look right and simplify javascript into discrete functions

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-$('#add').click(function() {
+function addGlobal() {
     var taskText = $('input:text').val();
     console.log(taskText);
     if (taskText != "") {
         $('input:text').val("");
         addTask(taskText);
     }
+}
+
+function complete(elem) {
+    var div = $(elem).closest('task');
+    div.css("background-color", "green");
+    div.fadeOut(200, "swing");
+}
+
+function cancel(elem) {
+    var div = $(elem).closest('task');
+    div.css("background-color", "#F83F56");
+    div.fadeOut(200, "swing");
+}
+
+$('#add').click(function() {
+    addGlobal();
 });
 
 $('#done').click(function() {
-    console.log("Remove: " + $(this).closest('task'));
-    $(this).closest('task').remove();
+    complete(this);
 });
 
+$("#cancel").click(function() {
+    cancel(this);
+});
 
 function addTask(txt) {
     var results = chrono.parse(txt);
@@ -28,10 +46,16 @@ function addTask(txt) {
     var openTimeDue = "</span><span class='dueTime'>";
     var openText = "</span></div><span class='taskText'>";
     var closeText = "</span>";
-    var buttonHTML = "<button id='done' class='btn btn-link'>Done</button></task>";
+    var buttonHTML = '<button id="done" class="btn btn-link"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button><button id="cancel" class="btn btn-link"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>'
     $('#tasks').append(openDateDue + dueDateFormatted + openTimeDue + dueTimeFormatted + openText + txt + closeText + buttonHTML);    
-    $("task:last-child").click(function() {    
-        $(this).closest('task').remove();    
+
+
+    $("task #done").click(function() {
+      complete(this);
+    });
+
+    $("task #cancel").click(function() {
+      cancel(this);
     });
 
 }
