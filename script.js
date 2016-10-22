affc308309efa5e6027dd90c7d5feed41a3e21f2
Henry Dixon <hdixon@cmu.edu>
2016-10-22 16:43:20 -0400

    cleanup all adding a new task

--- script.js
+++ script.js
@@ -8,26 +8,30 @@ $('#add').click(function() {
 });
 
 $('#done').click(function() {
-  console.log("Remove: " + $(this).closest('task'));
-  $(this).closest('task').remove();
+    console.log("Remove: " + $(this).closest('task'));
+    $(this).closest('task').remove();
 });
 
 
 function addTask(txt) {
-  var results = chrono.parse(txt);
-  if (results.length > 0) {
-    var dueDateRaw = results[0].start.date();
-    var dueDateFormatted = moment(dueDateRaw).format('dddd');
-    var dueTimeFormatted = moment(dueDateRaw).format('h a');
-  } else {
-    var dueDateFormatted = "";
-    var dueTimeFormatted = ""
-  }
+    var results = chrono.parse(txt);
+    if (results.length > 0) {
+        var dueDateRaw = results[0].start.date();
+        var dueDateFormatted = moment(dueDateRaw).format('dddd');
+        var dueTimeFormatted = moment(dueDateRaw).format('h a');
+    } else {
+        var dueDateFormatted = "";
+        var dueTimeFormatted = "";
+    }
 
-  $('#tasks').append("<task><div id='timeDue'><span class='dueDate'>" + dueDateFormatted + "</span><span class='dueTime'>" + dueTimeFormatted + "</span></div><span class='taskText'>" + txt + "<span><button id='done' class='btn btn-link'>Done</button></task>");
-      
-  $("task:last-child").click(function() {    
-      $(this).closest('task').remove();    
-  });
+    var openDateDue = "<task><div id='timeDue'><span class='dueDate'>";
+    var openTimeDue = "</span><span class='dueTime'>";
+    var openText = "</span></div><span class='taskText'>";
+    var closeText = "</span>";
+    var buttonHTML = "<button id='done' class='btn btn-link'>Done</button></task>";
+    $('#tasks').append(openDateDue + dueDateFormatted + openTimeDue + dueTimeFormatted + openText + txt + closeText + buttonHTML);    
+    $("task:last-child").click(function() {    
+        $(this).closest('task').remove();    
+    });
 
 }
